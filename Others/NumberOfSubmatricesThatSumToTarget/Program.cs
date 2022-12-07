b125e4169734bd22d1473f8c174c38d4af51d5dc
Tai Nguyen <tai.nguyen@trapets.com>
2022-12-07 09:06:01 +0700

    update

---
+++ Others/NumberOfSubmatricesThatSumToTarget/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NumberOfSubmatricesThatSumToTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[3][];
+            nums[0] = new int[] { 0, 1, 0 };
+            nums[1] = new int[] { 1, 1, 1 };
+            nums[2] = new int[] { 0, 1, 3 };
+            var result = NumSubmatrixSumTarget(nums, 0);
+            Console.WriteLine(result);
+        }
+        public static int NumSubmatrixSumTarget(int[][] matrix, int target)
+        {
+            int m = matrix.Length, n = matrix[0].Length;
+            var sum = new int[m + 1][];
+            for (int i = 0; i < m+1; i++)
+            {
+                sum[i] = new int[n + 1];
+            }
+            int count = 0;
+
+            for (int x = 1; x <= m; x++)
+            {
+                for (int y = 1; y <= n; y++)
+                {
+                    sum[x][y] = matrix[x - 1][y - 1] + sum[x - 1][y] + sum[x][y - 1] - sum[x - 1][y - 1];
+                    //Console.WriteLine($"{matrix[x - 1][y - 1]} + {sum[x - 1][y]} + {sum[x][y - 1]} - {sum[x - 1][y - 1]}");
+                    Console.WriteLine($"sum[{x}][{y}] = {sum[x][y]}");
+                    for (int i = 1; i <= x; i++)
+                    {
+                        for (int j = 1; j <= y; j++)
+                        {
+                            if (sum[x][j - 1] + sum[i - 1][y] - sum[i - 1][j - 1] + target == sum[x][y])
+                                count++;
+                        }
+                    }
+
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Strings/DetermineIfTwoStringsAreClose/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DetermineIfTwoStringsAreClose
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "caabbbzd";
+            string s2 = "baacccdz";
+            Console.WriteLine(CloseStrings(s1,s2));
+        }
+
+        public static bool CloseStrings(string word1, string word2)
+        {
+            HashSet<char> hs = new(word2); // O(1) - 26 chars max
+            var freq1 = new int[26];
+            var freq2 = new int[26];
+            if(word1.Length != word2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < word1.Length; i++)
+            {
+                freq1[word1[i] - 'a']++;
+                if (hs.Add(word1[i])) return false;
+                freq2[word2[i] - 'a']++;
+            }
+           
+            for (int i = 0; i < word2.Length; i++)
+            {
+                
+            }    
+              
+            Array.Sort(freq1); // O(1) - O(26*log26)
+            Array.Sort(freq2);
+
+            return freq1.SequenceEqual(freq2); // O(1) - 26 chars
+        }
+    }
+}
